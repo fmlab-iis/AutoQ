484daa0278106dedc38bedda382623a4a568ac9a
Wei-Lun Tsai <alan23273850@gmail.com>
2022-05-24 11:05:42 +0800

    add classical states test

--- unit_tests/explicit_tree_aut_test.cc
+++ unit_tests/explicit_tree_aut_test.cc
@@ -18,7 +18,7 @@
 #define BOOST_TEST_MODULE AutType
 #include <boost/test/unit_test.hpp>
 
-int size = 10;
+int size = 5;
 
 bool shellCmd(const string &cmd, string &result) {
     char buffer[512];
@@ -62,119 +62,146 @@ VATA::Util::TreeAutomata uniform(int n) {
 	aut.finalStates.insert(0);
     aut.stateNum = pow_of_two;
 
-    aut.determinize();
-    aut.minimize();
+    // aut.determinize();
+    // aut.minimize();
+    return aut;
+}
+
+VATA::Util::TreeAutomata classical(int n) {
+    VATA::Util::TreeAutomata aut;
+    aut.name = "Classical";
+
+    for (int level=1; level<=n; level++) {
+        if (level >= 2)
+            aut.transitions[{level}][{2*level - 1, 2*level - 1}] = {2*level - 3};
+        aut.transitions[{level}][{2*level - 1, 2*level}] = {2*level - 2};
+        aut.transitions[{level}][{2*level, 2*level - 1}] = {2*level - 2};
+        aut.symbols[{level}] = 2;
+    }
+    aut.transitions[{1,0,0,0,0}][{}] = {2*n};
+    aut.symbols[{1,0,0,0,0}] = 0;
+    aut.transitions[{0,0,0,0,0}][{}] = {2*n - 1};
+    aut.symbols[{0,0,0,0,0}] = 0;
+	aut.finalStates.insert(0);
+    aut.stateNum = 2*n + 1;
+
+    // aut.determinize();
+    // aut.minimize();
     return aut;
 }
 
 BOOST_AUTO_TEST_CASE(Z_gate_twice_to_identity)
 {
     VATA::Serialization::TimbukSerializer serializer;
-    VATA::Util::TreeAutomata before = uniform(size);
-    VATA::Util::TreeAutomata after = before;
-    
-    ofstream file1("/tmp/automata1.txt");
-    file1 << serializer.Serialize(before);
-    file1.close();
-    
-    for (int i=0; i<2; i++) {
-        after.Z(size/2);
+    for (const auto &before : {uniform(size), classical(size)}) {
+        VATA::Util::TreeAutomata after = before;
 
-        ofstream file2("/tmp/automata2.txt");
-        file2 << serializer.Serialize(after);
-        file2.close();
+        ofstream file1("/tmp/automata1.txt");
+        file1 << serializer.Serialize(before);
+        file1.close();
 
-        string include1, include2;
-        shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata1.txt /tmp/automata2.txt", include1);
-        shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata2.txt /tmp/automata1.txt", include2);
+        for (int i=0; i<2; i++) {
+            after.Z(size/2);
 
-        if (i < 2-1)
-            BOOST_REQUIRE_MESSAGE(!(include1=="1\n" && include2=="1\n"), "");
-        else
-            BOOST_REQUIRE_MESSAGE(include1=="1\n" && include2=="1\n", "");
-    }    
+            ofstream file2("/tmp/automata2.txt");
+            file2 << serializer.Serialize(after);
+            file2.close();
+
+            string include1, include2;
+            shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata1.txt /tmp/automata2.txt", include1);
+            shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata2.txt /tmp/automata1.txt", include2);
+
+            if (i < 2-1)
+                BOOST_REQUIRE_MESSAGE(!(include1=="1\n" && include2=="1\n"), "");
+            else
+                BOOST_REQUIRE_MESSAGE(include1=="1\n" && include2=="1\n", "");
+        }
+    }
 }
 
 BOOST_AUTO_TEST_CASE(S_gate_fourth_to_identity)
 {
     VATA::Serialization::TimbukSerializer serializer;
-    VATA::Util::TreeAutomata before = uniform(size);
-    VATA::Util::TreeAutomata after = before;
-    
-    ofstream file1("/tmp/automata1.txt");
-    file1 << serializer.Serialize(before);
-    file1.close();
+    for (const auto &before : {uniform(size), classical(size)}) {
+        VATA::Util::TreeAutomata after = before;
 
-    for (int i=0; i<4; i++) {
-        after.S(size/2);    
+        ofstream file1("/tmp/automata1.txt");
+        file1 << serializer.Serialize(before);
+        file1.close();
 
-        ofstream file2("/tmp/automata2.txt");
-        file2 << serializer.Serialize(after);
-        file2.close();
+        for (int i=0; i<4; i++) {
+            after.S(size/2);
 
-        string include1, include2;
-        shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata1.txt /tmp/automata2.txt", include1);
-        shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata2.txt /tmp/automata1.txt", include2);
+            ofstream file2("/tmp/automata2.txt");
+            file2 << serializer.Serialize(after);
+            file2.close();
 
-        if (i < 4-1)
-            BOOST_REQUIRE_MESSAGE(!(include1=="1\n" && include2=="1\n"), "");
-        else
-            BOOST_REQUIRE_MESSAGE(include1=="1\n" && include2=="1\n", "");
+            string include1, include2;
+            shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata1.txt /tmp/automata2.txt", include1);
+            shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata2.txt /tmp/automata1.txt", include2);
+
+            if (i < 4-1)
+                BOOST_REQUIRE_MESSAGE(!(include1=="1\n" && include2=="1\n"), "");
+            else
+                BOOST_REQUIRE_MESSAGE(include1=="1\n" && include2=="1\n", "");
+        }
     }
 }
 
 BOOST_AUTO_TEST_CASE(T_gate_eighth_to_identity)
 {
     VATA::Serialization::TimbukSerializer serializer;
-    VATA::Util::TreeAutomata before = uniform(size);
-    VATA::Util::TreeAutomata after = before;
-    
-    ofstream file1("/tmp/automata1.txt");
-    file1 << serializer.Serialize(before);
-    file1.close();
+    for (const auto &before : {uniform(size), classical(size)}) {
+        VATA::Util::TreeAutomata after = before;
 
-    for (int i=0; i<8; i++) {
-        after.T(size/2);    
+        ofstream file1("/tmp/automata1.txt");
+        file1 << serializer.Serialize(before);
+        file1.close();
 
-        ofstream file2("/tmp/automata2.txt");
-        file2 << serializer.Serialize(after);
-        file2.close();
+        for (int i=0; i<8; i++) {
+            after.T(size/2);
 
-        string include1, include2;
-        shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata1.txt /tmp/automata2.txt", include1);
-        shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata2.txt /tmp/automata1.txt", include2);
+            ofstream file2("/tmp/automata2.txt");
+            file2 << serializer.Serialize(after);
+            file2.close();
 
-        if (i < 8-1)
-            BOOST_REQUIRE_MESSAGE(!(include1=="1\n" && include2=="1\n"), "");
-        else
-            BOOST_REQUIRE_MESSAGE(include1=="1\n" && include2=="1\n", "");
+            string include1, include2;
+            shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata1.txt /tmp/automata2.txt", include1);
+            shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata2.txt /tmp/automata1.txt", include2);
+
+            if (i < 8-1)
+                BOOST_REQUIRE_MESSAGE(!(include1=="1\n" && include2=="1\n"), "");
+            else
+                BOOST_REQUIRE_MESSAGE(include1=="1\n" && include2=="1\n", "");
+        }
     }
 }
 
 BOOST_AUTO_TEST_CASE(CZ_gate_twice_to_identity)
 {
     VATA::Serialization::TimbukSerializer serializer;
-    VATA::Util::TreeAutomata before = uniform(size);
-    VATA::Util::TreeAutomata after = before;
-    
-    ofstream file1("/tmp/automata1.txt");
-    file1 << serializer.Serialize(before);
-    file1.close();
-    
-    for (int i=0; i<2; i++) {
-        after.CZ(size*2/3, size/3);
+    for (const auto &before : {uniform(size), classical(size)}) {
+        VATA::Util::TreeAutomata after = before;
 
-        ofstream file2("/tmp/automata2.txt");
-        file2 << serializer.Serialize(after);
-        file2.close();
+        ofstream file1("/tmp/automata1.txt");
+        file1 << serializer.Serialize(before);
+        file1.close();
 
-        string include1, include2;
-        shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata1.txt /tmp/automata2.txt", include1);
-        shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata2.txt /tmp/automata1.txt", include2);
+        for (int i=0; i<2; i++) {
+            after.CZ(size*2/3, size/3);
 
-        if (i < 2-1)
-            BOOST_REQUIRE_MESSAGE(!(include1=="1\n" && include2=="1\n"), "");
-        else
-            BOOST_REQUIRE_MESSAGE(include1=="1\n" && include2=="1\n", "");
-    }    
+            ofstream file2("/tmp/automata2.txt");
+            file2 << serializer.Serialize(after);
+            file2.close();
+
+            string include1, include2;
+            shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata1.txt /tmp/automata2.txt", include1);
+            shellCmd("/home/alan23273850/libvata/build/cli/vata incl /tmp/automata2.txt /tmp/automata1.txt", include2);
+
+            if (i < 2-1)
+                BOOST_REQUIRE_MESSAGE(!(include1=="1\n" && include2=="1\n"), "");
+            else
+                BOOST_REQUIRE_MESSAGE(include1=="1\n" && include2=="1\n", "");
+        }
+    }
 }
