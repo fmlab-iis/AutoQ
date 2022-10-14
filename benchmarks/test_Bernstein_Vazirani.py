3489af3d82fa97f38e489a11c91305970913864b
Wei-Lun Tsai <alan23273850@gmail.com>
2022-10-14 18:18:06 +0800

    add SliQSim test support

--- benchmarks/test_Bernstein_Vazirani.py
+++ benchmarks/test_Bernstein_Vazirani.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-
-from qiskit import QuantumCircuit
+UseSliQSim = False
+from qiskit import QuantumCircuit, execute
 from qiskit.providers.aer import Aer
+if UseSliQSim:
+    from qiskit_sliqsim_provider import SliQSimProvider # <--- import the provider
+    simulator = SliQSimProvider().get_backend('strong_simulator') # <--- use the provider
+else: simulator = Aer.get_backend('aer_simulator')
+gates = simulator.configuration().basis_gates
+gates.remove('rx')
+gates.remove('ry')
+gates.append('rx(pi/2)')
+gates.append('ry(pi/2)')
 
+# 正確答案的順序是由上填到下，1010...
 for i in range(1, 101):
-    text_file = open(f'Bernstein_Vazirani_{i:03}.qasm', "r")
-    data = text_file.read()
-    text_file.close()
-    qc = QuantumCircuit.from_qasm_str(data)
-    qc.save_statevector()
+    qc = QuantumCircuit.from_qasm_file(f'Bernstein_Vazirani_{i:03}.qasm')
+    if not UseSliQSim: qc.save_statevector() # <--- should be disabled in SliQSimProvider!
 
-    simulator = Aer.get_backend('aer_simulator')
-    job = simulator.run(qc, shots=1)
+    job = execute(qc, backend=simulator, shots=1, basis_gates=gates)
     result = job.result()
     # statevector = result.get_statevector(qc)
     # print(statevector)
