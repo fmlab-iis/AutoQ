5fae32344223f2e124ad2cf143d66e77ad2a0ad3
Wei-Lun Tsai <alan23273850@gmail.com>
2022-10-25 20:03:57 +0800

    modify the table a little

--- ArtifactEvaluation/from_file_to_table.py
+++ ArtifactEvaluation/from_file_to_table.py
@@ -3,17 +3,30 @@ import sys
 from qiskit import QuantumCircuit
 
 print(r'\begin{table}')
+print(r'\resizebox{\textwidth}{!}{')
 print(r'\begin{tabular}{|c||c|c||c|c|c|c|c|c||c|c|c|c|c|c||c||c|}\hline')
 current_benchmark = ''
 lst = []
+bm = {'BernsteinVazirani': 'Bernstein-Vazirani\'s algorithm with single oracle',  'Feynman': 'Feynman',
+    'Grover': 'Grover\'s algorithm with single oracle', 'MCToffoli': 'Multi-controlled Toffoli gate',
+    'MOGrover': 'Grover\'s algorithm with all possible oracles', 'Random': 'Random gates',
+    'RevLib': 'RevLib'}
 
 file = open(sys.argv[1], 'r')
 for line in file.readlines():
     # If AutoQ-P timeout, print #q and #G from the qasm file.
     if r'\multicolumn{8}{c||}{TO}' in line:
         qc = QuantumCircuit.from_qasm_file(f"{line.split(' & ')[0]}/circuit.qasm")
-        line = line.replace(r'\multicolumn{8}{c||}{TO}', f'{qc.num_qubits} & {qc.size()} & ' + r'\multicolumn{6}{c||}{TO}')
+        line = line.replace(r'\multicolumn{8}{c||}{TO}', f'{qc.num_qubits} & {qc.size()} & ' + r'\multicolumn{6}{c||}{\textbf{TO}}')
+    else:
+        line = ' & '.join(line.split(' & ')[:7] + [r'\textbf{' + line.split(' & ')[7] + '}'] + line.split(' & ')[8:])
+        if r'\multicolumn{6}{c||}{TO}' not in line:
+            line = ' & '.join(line.split(' & ')[:13] + [r'\textbf{' + line.split(' & ')[13] + '}'] + line.split(' & ')[14:])
+    line = line.replace(r'\multicolumn{6}{c||}{TO}', r'\multicolumn{6}{c||}{\textbf{TO}}')
+    line = ' & '.join(line.split(' & ')[:-2] + [r'\textbf{' + line.split(' & ')[-2] + '}'] + line.split(' & ')[-1:])
     line = line.strip().replace('_', r'\_').replace('^', r'\textasciicircum').replace('Equal (took ', '').replace('s)', 's')
+    line = line.replace('TO', r'$>$ 12m')
+    line = line.replace('ERR', 'ERROR')
     # Process Feynman format
     last = line.split(' & ')[-1]
     if last.endswith('s'):
@@ -33,9 +46,9 @@ for line in file.readlines():
         lst = []
     if line.split('/')[1] != current_benchmark:
         current_benchmark = line.split('/')[1]
-        print(r'\hline\multicolumn{17}{|c|}{\textbf{' + current_benchmark + '}}', end='\\\\\hline\n')
-        print('\\textbf{Problem} & \\textbf{\#q} & \\textbf{\#G} & \\textbf{P-sb} & \\textbf{P-sa} & \\textbf{P-tb} & \\textbf{P-ta} & \\textbf{P-simT} & \\textbf{P-verT} \
-& \\textbf{C-sb} & \\textbf{C-sa} & \\textbf{C-tb} & \\textbf{C-ta} & \\textbf{C-simT} & \\textbf{C-verT} \
+        print(r'\hline\multicolumn{17}{|c|}{\textbf{' + bm[current_benchmark] + '}}', end='\\\\\hline\n')
+        print('\\textbf{Problem} & \\textbf{\#q} & \\textbf{\#G} & \\textbf{P-sb} & \\textbf{P-sa} & \\textbf{P-tb} & \\textbf{P-ta} & \\textbf{P-exeT} & \\textbf{P-verT} \
+& \\textbf{C-sb} & \\textbf{C-sa} & \\textbf{C-tb} & \\textbf{C-ta} & \\textbf{C-exeT} & \\textbf{C-verT} \
 & \\textbf{qiskitT} & \\textbf{feynmanT}', end='\\\\\hline\n')
     if './Random/' in line:
         cols = line.split(' & ')
@@ -51,5 +64,6 @@ for line in file.readlines():
     else:
         print('/'.join(line.split('/')[2:]), end='\\\\\hline\n')
 
+print('}')
 print(r'\end{tabular}')
 print(r'\end{table}')
