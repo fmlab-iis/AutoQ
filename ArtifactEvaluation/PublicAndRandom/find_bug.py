d0bd27ff446100820ae40f56102e4e109ab29fe8
Wei-Lun Tsai <alan23273850@gmail.com>
2022-11-05 20:58:18 +0800

    finish PublicAndRandom script

--- ArtifactEvaluation/PublicAndRandom/find_bug.py
+++ ArtifactEvaluation/PublicAndRandom/find_bug.py
@@ -1,57 +1,89 @@
 #!/usr/bin/python3
-import os
-# Suggested Command: 2>&1 ./artifact_evaluation.py | tee /dev/tty > output.txt
+import os, subprocess, sys
+from ctypes import c_wchar_p
+from multiprocessing import Manager, Process, Semaphore
 
-timeout1 = 'XXX'
-timeout2 = 'XXX'
-timeout3 = 'XXX'
-timeout4 = 'XXX'
+name = sys.argv[1]
 
-for root, dirnames, filenames in sorted(os.walk('.')):
-    if ('Random' not in root) and ('Feynman' not in root) and ('RevLib' not in root): continue
-    if len(dirnames) == 0 and 'post.aut' in filenames:
-        print(root, end=' & ', flush=True)# , dirnames, filenames)
-        ###################################
-        # Tool 1 - AutoQ w.r.t. permutation
-        if timeout1 in root:
-            ret = 124
-        else:
-            ret = os.system(f'VATA_PATH=/home/alan23273850/libvata/build/cli/vata timeout 720 ../permutation {root}/pre.aut {root}/bug.qasm /tmp/automata.aut {root}/post.aut')
-            ret = (ret >> 8) & 0xff
+def AutoQ_permutation(root, str1, semaphore):
+    with semaphore:
+        another = root.replace('.', '').replace('/', '') + 'permutation'
+        p = subprocess.run(f'VATA_PATH=/home/alan23273850/libvata/build/cli/vata timeout 720 ../permutation {root}/pre.aut {root}/bug.qasm /tmp/{another}.aut {root}/post.aut', shell=True, capture_output=True, executable='/bin/bash')
+        ret = p.returncode
         if ret == 0:
-            print('', end=' & ', flush=True)
+            str1.value = list(map(lambda x: x.decode('utf-8'), p.stdout.splitlines()))[0] + ' & '
         elif ret == 124:
-            if ('Feynman' not in root) and ('RevLib' not in root):
-                timeout1 = '/'.join(root.split('/')[:-1])
-            print('\multicolumn{8}{c||}{TO}', end=' & ', flush=True)
+            str1.value = '\multicolumn{8}{c||}{TO} & '
         else:
-            print('\multicolumn{8}{c||}{ERR}', end=' & ', flush=True)
-        ###################################
-        # Tool 2 - AutoQ w.r.t. composition
-        if timeout2 in root:
-            ret = 124
-        else:
-            ret = os.system(f'VATA_PATH=/home/alan23273850/libvata/build/cli/vata timeout 720 ../composition {root}/pre.aut {root}/bug.qasm /tmp/automata.aut {root}/post.aut')
-            ret = (ret >> 8) & 0xff
+            str1.value = '\multicolumn{8}{c||}{ERR} & '
+        if os.path.exists(f'/tmp/{another}.aut'):
+            os.system(f'rm /tmp/{another}.aut')
+def AutoQ_composition(root, str2, semaphore):
+    with semaphore:
+        another = root.replace('.', '').replace('/', '') + 'composition'
+        p = subprocess.run(f'VATA_PATH=/home/alan23273850/libvata/build/cli/vata timeout 720 ../composition {root}/pre.aut {root}/bug.qasm /tmp/{another}.aut {root}/post.aut', shell=True, capture_output=True, executable='/bin/bash')
+        ret = p.returncode
         if ret == 0:
-            print('', end=' & ', flush=True)
+            str2.value = list(map(lambda x: x.decode('utf-8'), p.stdout.splitlines()))[0] + ' & '
         elif ret == 124:
-            if ('Feynman' not in root) and ('RevLib' not in root):
-                timeout2 = '/'.join(root.split('/')[:-1])
-            print('\multicolumn{6}{c||}{TO}', end=' & ', flush=True)
+            str2.value = '\multicolumn{6}{c||}{TO} & '
         else:
-            print('\multicolumn{6}{c||}{ERR}', end=' & ', flush=True)
-        ###################################
-        # Tool 4 - Feynman
-        if timeout4 in root:
-            ret = 124
-        else:
-            ret = os.system(f'timeout 720 /home/alan23273850/feynman/feynver {root}/circuit.qc {root}/bug.qc')
-            ret = (ret >> 8) & 0xff
+            str2.value = '\multicolumn{6}{c||}{ERR} & '
+        if os.path.exists(f'/tmp/{another}.aut'):
+            os.system(f'rm /tmp/{another}.aut')
+def Feynman(root, str3, semaphore):
+    with semaphore:
+        p = subprocess.run(f'timeout 720 /home/alan23273850/feynman/feynver {root}/circuit.qc {root}/bug.qc', shell=True, capture_output=True, executable='/bin/bash')
+        ret = p.returncode
         if ret == 124:
-            if ('Feynman' not in root) and ('RevLib' not in root):
-                timeout4 = '/'.join(root.split('/')[:-1])
-            print('TO', flush=True)
+            str3.value = '\multicolumn{2}{c|}{TO}'
         elif ret != 0:
-            print('ERR', flush=True)
-        ###################################
+            str3.value = '\multicolumn{2}{c|}{ERR}'
+        else:
+            tmp = list(map(lambda x: x.decode('utf-8'), p.stdout.splitlines()))[0]
+            str3.value = tmp.split(' (took ')[0] + ' & ' + tmp.split(' (took ')[1].split(')')[0]
+
+NUM_OF_THREADS = 240
+semaphore = Semaphore(NUM_OF_THREADS)
+manager = Manager()
+process_pool_large = []
+string_pool_large = []
+for root, dirnames, filenames in sorted(os.walk('.')):
+    if len(dirnames) == 0 and 'post.aut' in filenames:
+        process_pool_small = []
+        string_pool_small = [manager.Value(c_wchar_p, root + ' & '), manager.Value(c_wchar_p, ''), manager.Value(c_wchar_p, ''), manager.Value(c_wchar_p, '')]
+        semaphore.acquire(); semaphore.release()
+        p = Process(target=AutoQ_permutation, args=(root, string_pool_small[1], semaphore))
+        p.start()
+        process_pool_small.append(p)
+        semaphore.acquire(); semaphore.release()
+        p = Process(target=AutoQ_composition, args=(root, string_pool_small[2], semaphore))
+        p.start()
+        process_pool_small.append(p)
+        semaphore.acquire(); semaphore.release()
+        p = Process(target=Feynman, args=(root, string_pool_small[3], semaphore))
+        p.start()
+        process_pool_small.append(p)
+        process_pool_large.append((len(process_pool_large), process_pool_small))
+        string_pool_large.append(string_pool_small)
+
+while len(process_pool_large) > 0:
+    for i, pps in enumerate(process_pool_large):
+        all_finish = True
+        for p in pps[1]:
+            if p.is_alive():
+                all_finish = False
+                break
+        if all_finish:
+            print(''.join(map(lambda x: x.value, string_pool_large[pps[0]])))
+            process_pool_large.pop(i)
+            break
+
+os.system('pkill -9 -f permutation')
+os.system('pkill -9 -f composition')
+os.system('pkill -9 -f feynver')
+
+f = open(name, 'w')
+for row in string_pool_large:
+    print(''.join(map(lambda x: x.value, row)), file=f)
+f.close()
