f13033159b95911a9231a9695cea49d7c984f9b6
Wei-Lun Tsai <alan23273850@gmail.com>
2022-11-08 23:52:27 +0800

    finish 1 try of QuantumAlgorithms

--- ArtifactEvaluation/QuantumAlgorithms/from_file_to_table.py
+++ ArtifactEvaluation/QuantumAlgorithms/from_file_to_table.py
@@ -2,9 +2,7 @@
 import sys
 from qiskit import QuantumCircuit
 
-# print(r'\begin{table}')
-print(r'\resizebox*{!}{\dimexpr\textheight-4\baselineskip\relax}{')
-print(r'\begin{tabular}{|c||c|c||c|c|c|c|c|c||c|c|c|c|c|c||c||c|}\hline')
+print(r'\begin{tabular}{|c||c|c||c|c|c|c|c|c||c|c|c|c|c|c||c||c|c|}\hline')
 current_benchmark = ''
 lst = []
 bm = {'BernsteinVazirani': 'Bernstein-Vazirani\'s algorithm with one hidden string',  'Feynman': 'Feynman',
@@ -15,19 +13,21 @@ bm = {'BernsteinVazirani': 'Bernstein-Vazirani\'s algorithm with one hidden stri
 file = open(sys.argv[1], 'r')
 for line in file.readlines():
     # Ignore the row where all tools are timeout or fail.
-    if (r'\multicolumn{8}{c||}{TO} & \multicolumn{6}{c||}{TO}' in line) and line.endswith('TO\n'):
+    line = line.replace('{AutoQ_composition}', '{6}')
+    line = line.replace('{Feynman}', '{2}')
+    if (r'\multicolumn{AutoQ_permutation}{c||}{TO} & \multicolumn{6}{c||}{TO}' in line) and line.endswith('\multicolumn{Feynman}{c|}{TO}\n'):
         continue
     # If AutoQ-P timeout, print #q and #G from the qasm file.
-    if r'\multicolumn{8}{c||}{TO}' in line:
+    if r'\multicolumn{AutoQ_permutation}{c||}{TO}' in line:
         qc = QuantumCircuit.from_qasm_file(f"{line.split(' & ')[0]}/circuit.qasm")
-        line = line.replace(r'\multicolumn{8}{c||}{TO}', f'{qc.num_qubits} & {qc.size()} & ' + r'\multicolumn{6}{c||}{\textbf{TO}}')
+        line = line.replace(r'\multicolumn{AutoQ_permutation}{c||}{TO}', f'{qc.num_qubits} & {qc.size()} & ' + r'\multicolumn{6}{c||}{\textbf{TO}}')
     else:
         line = ' & '.join(line.split(' & ')[:7] + [r'\textbf{' + line.split(' & ')[7] + '}'] + line.split(' & ')[8:])
         if r'\multicolumn{6}{c||}{TO}' not in line:
             line = ' & '.join(line.split(' & ')[:13] + [r'\textbf{' + line.split(' & ')[13] + '}'] + line.split(' & ')[14:])
     line = line.replace(r'\multicolumn{6}{c||}{TO}', r'\multicolumn{6}{c||}{\textbf{TO}}')
     line = ' & '.join(line.split(' & ')[:-2] + [r'\textbf{' + line.split(' & ')[-2] + '}'] + line.split(' & ')[-1:])
-    line = line.strip().replace('_', r'\_').replace('^', r'\textasciicircum').replace('Equal (took ', '').replace('s)', 's')
+    line = line.strip().replace('_', r'\_').replace('^', r'\textasciicircum')#.replace('Equal (took ', '').replace('s)', 's')
     line = line.replace('TO', r'$>$ 12m')
     line = line.replace('ERR', 'ERROR')
     # Process Feynman format
@@ -49,24 +49,10 @@ for line in file.readlines():
         lst = []
     if line.split('/')[1] != current_benchmark:
         current_benchmark = line.split('/')[1]
-        print(r'\hline\multicolumn{17}{|c|}{\textbf{' + bm[current_benchmark] + '}}', end='\\\\\hline\n')
+        print(r'\hline\multicolumn{18}{|c|}{\textbf{' + bm[current_benchmark] + '}}', end='\\\\\hline\n')
         print('\\textbf{Problem} & \\textbf{\#q} & \\textbf{\#G} & \\textbf{P-sb} & \\textbf{P-sa} & \\textbf{P-tb} & \\textbf{P-ta} & \\textbf{P-exeT} & \\textbf{P-incT} \
 & \\textbf{C-sb} & \\textbf{C-sa} & \\textbf{C-tb} & \\textbf{C-ta} & \\textbf{C-exeT} & \\textbf{C-incT} \
-& \\textbf{qiskitT} & \\textbf{feynmanT}', end='\\\\\hline\n')
-    if './Random/' in line:
-        cols = line.split(' & ')
-        t = 0; i = 15 if './Random/35' in line else 7
-        # for i in (7, 8, 13, 14, 15, 16):
-        cell = cols[i][8:-1]
-        if cell.endswith('s'):
-            cell = cell[:-1]
-            if 'm' in cell:
-                t += int(cell.split('m')[0]) * 60 + float(cell.split('m')[1])
-            else:
-                t += float(cell)
-        lst.append((t, line))
-    else:
-        print('/'.join(line.split('/')[2:]), end='\\\\\hline\n')
+& \\textbf{SliQSim} & \\textbf{feynmanV} & \\textbf{feynmanT}', end='\\\\\hline\n')
+    print('/'.join(line.split('/')[2:]), end='\\\\\hline\n')
 
-print(r'\end{tabular}}')
-# print(r'\end{table}')
+print(r'\end{tabular}')
---
+++ ArtifactEvaluation/QuantumAlgorithms/test_SliQSim.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+import os, re, subprocess, sys
+
+root = sys.argv[1]
+
+if 'MCToffoli' in root:
+    n = int(re.findall('[0-9]+', root)[-1]) # half of the number of qubits
+    with open(f'{root}/circuit.qasm', 'r') as file:
+        data = file.read()
+    total_time = 0
+    p2n = pow(2, n)
+    root2 = root[len('./'):]
+    os.system(f'mkdir -p /tmp/{root2}')
+    for i in range(p2n):
+        tmpstr = ''
+        if (i >> 0) & 1:
+            tmpstr += f'x qubits[{0}]; '
+        for j in range(1, n):
+            if (i >> j) & 1:
+                tmpstr += f'x qubits[{2*j-1}]; '
+        qc = re.sub(r'qreg qubits\[\d+\];', f'\g<0> {tmpstr}', data)
+        text_file = open(f'/tmp/{root2}/circuit.qasm', "w")
+        text_file.write(qc)
+        text_file.close()
+        p = subprocess.run(f'/home/alan23273850/SliQSim/SliQSim --sim_qasm /tmp/{root2}/circuit.qasm --type 1 --print_info', shell=True, capture_output=True, executable='/bin/bash')
+        # Assume the 1st line format - Runtime: 0.013964 seconds
+        tmp = list(map(lambda x: x.decode('utf-8'), p.stdout.splitlines()))[0].strip()
+        total_time += float(tmp.split(' ')[1])
+    os.system(f'rm -r /tmp/{root2}')
+    if total_time >= 60:
+        print('%dm%.fs' % (int(total_time // 60), total_time % 60), end='', flush=True)
+    elif total_time >= 10:
+        print('%.fs' % total_time, end='', flush=True)
+    else:
+        print('%.1fs' % total_time, end='', flush=True)
+elif 'MOGrover' in root:
+    n = int(re.findall('[0-9]+', root)[-1])
+    with open(f'{root}/circuit.qasm', 'r') as file:
+        data = file.read()
+    total_time = 0
+    p2n = pow(2, n)
+    root2 = root[len('./'):]
+    os.system(f'mkdir -p /tmp/{root2}')
+    for i in range(p2n):
+        tmpstr = ''
+        for j in range(n):
+            if (i >> j) & 1:
+                tmpstr += f'x qubits[{j}]; '
+        qc = re.sub(r'qreg qubits\[\d+\];', f'\g<0> {tmpstr}', data)
+        text_file = open(f'/tmp/{root2}/circuit.qasm', "w")
+        text_file.write(qc)
+        text_file.close()
+        p = subprocess.run(f'/home/alan23273850/SliQSim/SliQSim --sim_qasm /tmp/{root2}/circuit.qasm --type 1 --print_info', shell=True, capture_output=True, executable='/bin/bash')
+        # Assume the 1st line format - Runtime: 0.013964 seconds
+        tmp = list(map(lambda x: x.decode('utf-8'), p.stdout.splitlines()))[0].strip()
+        total_time += float(tmp.split(' ')[1])
+    os.system(f'rm -r /tmp/{root2}')
+    if total_time >= 60:
+        print('%dm%.fs' % (int(total_time // 60), total_time % 60), end='', flush=True)
+    elif total_time >= 10:
+        print('%.fs' % total_time, end='', flush=True)
+    else:
+        print('%.1fs' % total_time, end='', flush=True)
+else:
+    p = subprocess.run(f'/home/alan23273850/SliQSim/SliQSim --sim_qasm {root}/circuit.qasm --type 1 --print_info', shell=True, capture_output=True, executable='/bin/bash')
+    # Assume the 1st line format - Runtime: 0.013964 seconds
+    tmp = list(map(lambda x: x.decode('utf-8'), p.stdout.splitlines()))[0].strip()
+    total_time = float(tmp.split(' ')[1])
+    if total_time >= 60:
+        print('%dm%.fs' % (int(total_time // 60), total_time % 60), end='', flush=True)
+    elif total_time >= 10:
+        print('%.fs' % total_time, end='', flush=True)
+    else:
+        print('%.1fs' % total_time, end='', flush=True)
