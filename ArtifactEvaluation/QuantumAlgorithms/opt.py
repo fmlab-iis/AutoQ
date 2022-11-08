265e946d8e8e6a64b2ed1e3ab8592398fab200f9
Wei-Lun Tsai <alan23273850@gmail.com>
2022-11-08 22:44:30 +0800

    Revert "add opt.qasm in QuantumAlgorithms"
    This reverts commit 22f50ba6d5c2a786acb52b5db3bba767bb451a26.

--- ArtifactEvaluation/QuantumAlgorithms/opt.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import os
-
-for root, dirnames, filenames in sorted(os.walk('.')):
-    if len(dirnames) == 0 and 'circuit.qasm' in filenames:
-        os.system(f'/home/alan23273850/feynman/feynopt {root}/circuit.qasm > {root}/opt.qasm')
-    # if len(dirnames) == 0 and 'circuit.qc' in filenames:
-    #     os.system(f'/home/alan23273850/feynman/feynopt {root}/circuit.qc > {root}/opt.qc')
