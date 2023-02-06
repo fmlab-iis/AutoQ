b2672313b43297d5fdef6ecd1aa272c4f5a8054a
Wei-Lun Tsai <alan23273850@gmail.com>
2023-02-06 19:27:05 +0800

    finish 1st version of High-Level Specification Language

--- cli/auto_from_states.cc
+++ cli/auto_from_states.cc
@@ -4,36 +4,56 @@ using namespace std;
 using AUTOQ::Util::TreeAutomata;
 
 int main(int argc, char **argv) {
-    TreeAutomata aut;
-    for (int z=1; z<argc; z++) {
-        TreeAutomata aut2;
-        int cnt = 0;
-        while (true) {
-            char ch = argv[z][cnt++];
-            if (ch == '0') {
-                if (cnt > 1)
-                    aut2.transitions[cnt][{2*cnt-1, 2*cnt-1}] = {2*(cnt-1)-1};
-                aut2.transitions[cnt][{2*cnt, 2*cnt-1}] = {2*(cnt-1)};
+    TreeAutomata aut_final;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        TreeAutomata aut;
+        std::istringstream iss(line);
+        std::vector<TreeAutomata::InitialSymbol> states_probs;
+        TreeAutomata::InitialSymbol default_prob;
+        for (std::string state_prob; iss >> state_prob;) {
+            // std::cout << state_prob << "\n";
+            std::istringstream iss2(state_prob);
+            std::string state;
+            std::getline(iss2, state, ':');
+            if (states_probs.empty()) {
+                aut.qubitNum = state.length();
+                states_probs.resize(pow(2, state.length()));
             }
-            else if (ch == '1') {
-                if (cnt > 1)
-                    aut2.transitions[cnt][{2*cnt-1, 2*cnt-1}] = {2*(cnt-1)-1};
-                aut2.transitions[cnt][{2*cnt-1, 2*cnt}] = {2*(cnt-1)};
-            }
-            else {
-                cnt--;
-                aut2.transitions[{0,0,0,0,0}][{}] = {2*cnt-1};
-                aut2.transitions[{1,0,0,0,0}][{}] = {2*cnt};
-                aut2.finalStates = {0};
-                aut2.stateNum = 2 * cnt;
-                aut2.qubitNum = cnt;
-                break;
+            std::string t;
+            if (state == "*") {
+                while (std::getline(iss2, t, ',')) {
+                    default_prob.push_back(std::atoi(t.c_str()));
+                }
+            } else {
+                int s = std::stoi(state, nullptr, 2);
+                while (std::getline(iss2, t, ',')) {
+                    states_probs[s].push_back(std::atoi(t.c_str()));
+                }
             }
         }
-        aut = aut.Union(aut2);
+        int pow_of_two = 1;
+        TreeAutomata::State state_counter = 0;
+        for (int level=1; level<=aut.qubitNum; level++) {
+            for (int i=0; i<pow_of_two; i++) {
+                aut.transitions[{level}][{state_counter*2+1, state_counter*2+2}] = {state_counter};
+                state_counter++;
+            }
+            pow_of_two *= 2;
+        }
+        for (TreeAutomata::State i=state_counter; i<=state_counter*2; i++) {
+            if (states_probs[i-state_counter].empty())
+                aut.transitions[default_prob][{}].insert(i);
+            else
+                aut.transitions[states_probs[i-state_counter]][{}].insert(i);
+        }
+        aut.finalStates.push_back(0);
+        aut.stateNum = state_counter*2 + 1;
+        aut_final = aut_final.Union(aut);
+        aut_final.reduce();
     }
-    aut.fraction_simplification();
-    aut.reduce();
-    aut.print();
+    aut_final.fraction_simplification();
+    aut_final.reduce();
+    aut_final.print();
     return 0;
 }
