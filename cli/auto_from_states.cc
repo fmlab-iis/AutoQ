425b0b61d3d4a899c849d73b401a56210711f41e
Wei-Lun Tsai <alan23273850@gmail.com>
2023-02-08 14:11:56 +0800

    finish bounded variable |i|=n of HLSL

--- cli/auto_from_states.cc
+++ cli/auto_from_states.cc
@@ -3,7 +3,7 @@
 using namespace std;
 using AUTOQ::Util::TreeAutomata;
 
-TreeAutomata from_string_to_automaton(std::string tree) {
+TreeAutomata from_tree_to_automaton(std::string tree) {
     TreeAutomata aut;
     std::istringstream iss(tree);
     std::map<TreeAutomata::State, TreeAutomata::InitialSymbol> states_probs;
@@ -50,68 +50,103 @@ TreeAutomata from_string_to_automaton(std::string tree) {
     return aut;
 }
 
+TreeAutomata from_line_to_automaton(std::string line) {
+    std::istringstream iss_tensor(line);
+    std::string tree;
+    std::getline(iss_tensor, tree, '#');
+
+    auto aut = from_tree_to_automaton(tree); // the first automata to be tensor producted
+
+    // to tensor product with the rest of the automata
+    while (std::getline(iss_tensor, tree, '#')) {
+        auto aut2 = from_tree_to_automaton(tree);
+
+        // let aut2 be tensor producted with aut here
+        TreeAutomata::TransitionMap aut_leaves;
+        for (const auto &t : aut.transitions) {
+            if (t.first.is_leaf()) {
+                aut_leaves[t.first] = t.second;
+            }
+        }
+        for (const auto &t : aut_leaves) {
+            aut.transitions.erase(t.first);
+        }
+
+        // append aut2 to each leaf transition of aut
+        for (const auto &t : aut_leaves) {
+            for (const auto &t2 : aut2.transitions) {
+                if (t2.first.is_internal()) { // if the to-be-appended transition is internal, then
+                    auto Q = aut.qubitNum + t2.first.initial_symbol().at(0); // we need to shift the qubit number
+                    for (const auto &kv : t2.second) { // for each pair of vec_in -> set_out
+                        auto k = kv.first;
+                        for (int i=0; i<k.size(); i++)
+                            k.at(i) += aut.stateNum;
+                        // above shift the state number of vec_in first,
+                        for (const auto &s : kv.second) {
+                            if (s == 0) { // if to be connected to leaf states of aut, then
+                                for (const auto &s2 : t.second.at({})) // simply apply these states
+                                    aut.transitions[{Q}][k].insert(s2);
+                            }
+                            else // and then shift the state number of set_out
+                                aut.transitions[{Q}][k].insert(s + aut.stateNum);
+                        }
+                    }
+                } else {
+                    for (const auto &kv : t2.second) {
+                        auto k = kv.first;
+                        for (int i=0; i<k.size(); i++)
+                            k.at(i) += aut.stateNum;
+                        for (const auto &s : kv.second) {
+                            aut.transitions[t.first.initial_symbol() * t2.first.initial_symbol()][k].insert(s + aut.stateNum);
+                        }
+                    }
+                }
+            }
+            aut.stateNum += aut2.stateNum;
+        }
+        aut.qubitNum += aut2.qubitNum;
+        aut.reduce();
+    }
+    return aut;
+}
+
 int main(int argc, char **argv) {
     TreeAutomata aut_final;
     std::string line;
     while (std::cout << "> " && std::getline(std::cin, line)) {
-        std::istringstream iss_tensor(line);
-        std::string tree;
-        std::getline(iss_tensor, tree, '#');
+        if (line.substr(0, 4) == "|i|=") { // if startswith "|i|="
+            std::istringstream iss(line);
+            std::string length; iss >> length; length = length.substr(4);
+            line.clear();
+            for (std::string t; iss >> t;)
+                line += t + ' ';
+            std::string i(std::atoi(length.c_str()), '1');
+            bool reach_all_zero;
+            do {
+                auto aut = from_line_to_automaton(std::regex_replace(line, std::regex("i"), i));
+                aut_final = aut_final.Union(aut);
+                aut_final.reduce();
 
-        auto aut = from_string_to_automaton(tree); // the first automata to be tensor producted
-
-        // to tensor product with the rest of the automata
-        while (std::getline(iss_tensor, tree, '#')) {
-            auto aut2 = from_string_to_automaton(tree);
-
-            // let aut2 be tensor producted with aut here
-            TreeAutomata::TransitionMap aut_leaves;
-            for (const auto &t : aut.transitions) {
-                if (t.first.is_leaf()) {
-                    aut_leaves[t.first] = t.second;
-                }
-            }
-            for (const auto &t : aut_leaves) {
-                aut.transitions.erase(t.first);
-            }
-
-            // append aut2 to each leaf transition of aut
-            for (const auto &t : aut_leaves) {
-                for (const auto &t2 : aut2.transitions) {
-                    if (t2.first.is_internal()) { // if the to-be-appended transition is internal, then
-                        auto Q = aut.qubitNum + t2.first.initial_symbol().at(0); // we need to shift the qubit number
-                        for (const auto &kv : t2.second) { // for each pair of vec_in -> set_out
-                            auto k = kv.first;
-                            for (int i=0; i<k.size(); i++)
-                                k.at(i) += aut.stateNum;
-                            // above shift the state number of vec_in first,
-                            for (const auto &s : kv.second) {
-                                if (s == 0) { // if to be connected to leaf states of aut, then
-                                    for (const auto &s2 : t.second.at({})) // simply apply these states
-                                        aut.transitions[{Q}][k].insert(s2);
-                                }
-                                else // and then shift the state number of set_out
-                                    aut.transitions[{Q}][k].insert(s + aut.stateNum);
-                            }
+                // the following performs -1 on the binary string i
+                reach_all_zero = false;
+                for (int j=i.size()-1; j>=0; j--) {
+                    if (i.at(j) == '0') {
+                        if (j == 0) {
+                            reach_all_zero = true;
+                            break;
                         }
+                        i.at(j) = '1';
                     } else {
-                        for (const auto &kv : t2.second) {
-                            auto k = kv.first;
-                            for (int i=0; i<k.size(); i++)
-                                k.at(i) += aut.stateNum;
-                            for (const auto &s : kv.second) {
-                                aut.transitions[t.first.initial_symbol() * t2.first.initial_symbol()][k].insert(s + aut.stateNum);
-                            }
-                        }
+                        i.at(j) = '0';
+                        break;
                     }
                 }
-                aut.stateNum += aut2.stateNum;
-            }
-            aut.qubitNum += aut2.qubitNum;
-            aut.reduce();
+            } while (!reach_all_zero);
+        } else {
+            auto aut = from_line_to_automaton(line);
+            aut_final = aut_final.Union(aut);
+            aut_final.reduce();
         }
-        aut_final = aut_final.Union(aut);
-        aut_final.reduce();
     }
     aut_final.fraction_simplification();
     aut_final.reduce();
