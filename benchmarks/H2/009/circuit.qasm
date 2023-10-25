b1349f8d4803cde0ecd5cd1cffa5f16cce5276b4
Wei-Lun Tsai <alan23273850@gmail.com>
2023-10-25 22:49:07 +0800

    add H2 benchmark

---
+++ benchmarks/H2/009/circuit.qasm
@@ -0,0 +1,22 @@
+OPENQASM 2.0;
+include "qelib1.inc";
+qreg qubits[9];
+
+h qubits[8];
+h qubits[8];
+h qubits[7];
+h qubits[7];
+h qubits[6];
+h qubits[6];
+h qubits[5];
+h qubits[5];
+h qubits[4];
+h qubits[4];
+h qubits[3];
+h qubits[3];
+h qubits[2];
+h qubits[2];
+h qubits[1];
+h qubits[1];
+h qubits[0];
+h qubits[0];
---
+++ benchmarks/H2/produce_h2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import os
+
+for n in range(1, 1000):
+    folder = str(n).zfill(3)
+    os.mkdir(folder)
+    os.chdir(folder)
+
+    #########################################
+    with open("circuit.qasm", "w") as file:
+        file.write("OPENQASM 2.0;\n")
+        file.write('include "qelib1.inc";\n')
+        file.write(f'qreg qubits[{n}];\n\n')
+
+        for i in range(n-1, -1, -1):
+            file.write(f'h qubits[{i}];\n')
+            file.write(f'h qubits[{i}];\n')
+    #########################################
+
+    #########################################
+    with open("pre.aut", "w") as file:
+        file.write("Ops " + ' '.join(f'[{i}]:2' for i in range(1, n+1)) + ' [0,0,0,0,0]:0 [1,0,0,0,0]:0\n')
+        file.write("Automaton Basis\n")
+        file.write(f"States {' '.join([str(i) for i in range(2*n + 1)])}\n")
+        file.write("Final States 0\n")
+        file.write("Transitions\n")
+        for level in range(1, n+1):
+            if (level >= 2):
+                file.write(f"[{level}]({2*level - 1}, {2*level - 1}) -> {2*level - 3}\n")
+            file.write(f"[{level}]({2*level - 1}, {2*level}) -> {2*level - 2}\n")
+            file.write(f"[{level}]({2*level}, {2*level - 1}) -> {2*level - 2}\n")
+        file.write(f"[0,0,0,0,0] -> {2*n-1}\n")
+        file.write(f"[1,0,0,0,0] -> {2*n}\n")
+    #########################################
+
+    os.chdir('..')
