05d172eeb3affb90853e98a9723d5d963d82dbc9
Wei-Lun Tsai <alan23273850@gmail.com>
2023-11-09 01:12:47 +0800

    add GHZzero post.aut

--- benchmarks/GHZzero/produce_ghzzero.py
+++ benchmarks/GHZzero/produce_ghzzero.py
@@ -32,4 +32,23 @@ for n in range(2, 1000):
         file.write(f"[1,0,0,0,0] -> {2*n}\n")
     #########################################
 
+    #########################################
+    with open("post.aut", "w") as file:
+        file.write("Ops " + ' '.join(f'[{i}]:2' for i in range(1, n+1)) + ' [0,0,0,0,0]:0 [1,0,0,0,1]:0\n')
+        file.write("Automaton GHZzero_post\n")
+        file.write(f"States {' '.join([str(i) for i in range(3 * n)])}\n")
+        file.write("Final States 0\n")
+        file.write("Transitions\n")
+        file.write("[1](1, 2) -> 0\n")
+        file.write("[2](4, 3) -> 1\n")
+        file.write("[2](3, 5) -> 2\n")
+        for level in range(3, n+1):
+            file.write(f"[{level}]({(level - 1) * 3}, {(level - 1) * 3}) -> {(level - 2) * 3}\n")
+            file.write(f"[{level}]({(level - 1) * 3 + 1}, {(level - 1) * 3}) -> {(level - 2) * 3 + 1}\n")
+            file.write(f"[{level}]({(level - 1) * 3}, {(level - 1) * 3 + 2}) -> {(level - 2) * 3 + 2}\n")
+        file.write(f"[0,0,0,0,0] -> {(n - 1) * 3}\n")
+        file.write(f"[1,0,0,0,1] -> {(n - 1) * 3 + 1}\n")
+        file.write(f"[1,0,0,0,1] -> {(n - 1) * 3 + 2}\n")
+    #########################################
+
     os.chdir('..')
