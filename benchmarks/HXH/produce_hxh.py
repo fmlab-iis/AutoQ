3c876f613fe31d1874f39b69f9eda89aa8861c94
Wei-Lun Tsai <alan23273850@gmail.com>
2023-11-09 23:42:28 +0800

    add HXH post.aut

--- benchmarks/HXH/produce_hxh.py
+++ benchmarks/HXH/produce_hxh.py
@@ -34,4 +34,24 @@ for n in range(1, 100):
         file.write(f"[1,0,0,0,0] -> {2*n}\n")
     #########################################
 
+    #########################################
+    with open("post.aut", "w") as file:
+        file.write("Ops " + ' '.join(f'[{i}]:2' for i in range(1, n+1)) + ' [0,0,0,0,0]:0 [1,0,0,0,0]:0 [-1,0,0,0,0]:0\n')
+        file.write("Automaton HXH_post\n")
+        file.write(f"States {' '.join([str(i) for i in range(3*n + 1)])}\n")
+        file.write("Final States 0\n")
+        file.write("Transitions\n")
+        file.write(f"[1](1, 3) -> 0\n")
+        file.write(f"[1](2, 1) -> 0\n")
+        for i in range(2, n+1):
+            file.write(f"[{i}]({3*i - 2}, {3*i - 2}) -> {3*i - 5}\n")
+            file.write(f"[{i}]({3*i - 2}, {3*i    }) -> {3*i - 4}\n")
+            file.write(f"[{i}]({3*i - 1}, {3*i - 2}) -> {3*i - 4}\n")
+            file.write(f"[{i}]({3*i - 2}, {3*i - 1}) -> {3*i - 3}\n")
+            file.write(f"[{i}]({3*i    }, {3*i - 2}) -> {3*i - 3}\n")
+        file.write(f"[0,0,0,0,0] -> {3*n - 2}\n")
+        file.write(f"[1,0,0,0,0] -> {3*n - 1}\n")
+        file.write(f"[-1,0,0,0,0] -> {3*n    }\n")
+    #########################################
+
     os.chdir('..')
