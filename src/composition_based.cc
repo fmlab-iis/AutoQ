5ce606712ff3cafc0c17b2ef011756ee7dc9009f
Wei-Lun Tsai <alan23273850@gmail.com>
2024-03-29 15:27:04 +0800

    fix operator||

--- src/composition_based.cc
+++ src/composition_based.cc
@@ -575,44 +575,6 @@ void AUTOQ::Automata<Symbol>::value_restriction(int k, bool branch) {
     if (opLog) std::cout << __FUNCTION__ << "：" << stateNum << " states " << count_transitions() << " transitions\n";
 }
 
-template <typename Symbol>
-AUTOQ::Automata<Symbol> AUTOQ::Automata<Symbol>::operator||(const Automata<Symbol> &o) const {
-    if (this->qubitNum == 0) return o;
-
-    Automata<Symbol> result;
-    result = *this;
-    result.name = "operator||";
-    if (result.qubitNum != o.qubitNum) {
-        throw std::runtime_error(AUTOQ_LOG_PREFIX + "Two automata of different numbers of qubits cannot be unioned together.");
-    }
-    result.stateNum += o.stateNum;
-    // TODO: How to check if the two input automata have different k's?
-
-    for (const auto &t : o.transitions) {
-        auto &container = result.transitions[t.first];
-        for (const auto &out_ins : t.second) {
-            auto out = out_ins.first;
-            out += this->stateNum;
-            auto &sub_container = container[out];
-            for (auto in : out_ins.second) {
-                for (unsigned i=0; i<in.size(); i++) {
-                    in[i] += this->stateNum;
-                }
-                sub_container.insert(in);
-            }
-        }
-    }
-    for (const auto &s : o.finalStates) {
-        result.finalStates.insert(s + this->stateNum);
-    }
-    result.reduce();
-    for (const auto &var : o.vars)
-        result.vars.insert(var);
-    result.constraints += o.constraints;
-    if (opLog) std::cout << __FUNCTION__ << "：" << stateNum << " states " << count_transitions() << " transitions\n";
-    return result;
-}
-
 // https://bytefreaks.net/programming-2/c/c-undefined-reference-to-templated-class-function
 template struct AUTOQ::Automata<AUTOQ::Symbol::Concrete>;
 template struct AUTOQ::Automata<AUTOQ::Symbol::Symbolic>;
---
+++ src/general.cc
@@ -0,0 +1,50 @@
+#include "autoq/aut_description.hh"
+#include "autoq/symbol/concrete.hh"
+#include "autoq/symbol/symbolic.hh"
+#include "autoq/symbol/predicate.hh"
+#include <numeric> // used in std::numeric_limits
+#include <chrono> // used in remove_useless
+#include <queue> // used in remove_useless
+
+template <typename Symbol>
+AUTOQ::Automata<Symbol> AUTOQ::Automata<Symbol>::operator||(const Automata<Symbol> &o) const {
+    if (this->qubitNum == 0) return o;
+
+    Automata<Symbol> result;
+    result = *this;
+    result.name = "operator||";
+    if (result.qubitNum != o.qubitNum) {
+        throw std::runtime_error(AUTOQ_LOG_PREFIX + "Two automata of different numbers of qubits cannot be unioned together.");
+    }
+    result.stateNum += o.stateNum;
+    // TODO: How to check if the two input automata have different k's?
+
+    for (const auto &t : o.transitions) {
+        auto &container = result.transitions[t.first];
+        for (const auto &out_ins : t.second) {
+            auto out = out_ins.first;
+            out += this->stateNum;
+            auto &sub_container = container[out];
+            for (auto in : out_ins.second) {
+                for (unsigned i=0; i<in.size(); i++) {
+                    in[i] += this->stateNum;
+                }
+                sub_container.insert(in);
+            }
+        }
+    }
+    for (const auto &s : o.finalStates) {
+        result.finalStates.insert(s + this->stateNum);
+    }
+    result.reduce();
+    for (const auto &var : o.vars)
+        result.vars.insert(var);
+    result.constraints += o.constraints;
+    if (opLog) std::cout << __FUNCTION__ << "：" << stateNum << " states " << count_transitions() << " transitions\n";
+    return result;
+}
+
+// https://bytefreaks.net/programming-2/c/c-undefined-reference-to-templated-class-function
+template struct AUTOQ::Automata<AUTOQ::Symbol::Concrete>;
+template struct AUTOQ::Automata<AUTOQ::Symbol::Symbolic>;
+template struct AUTOQ::Automata<AUTOQ::Symbol::Predicate>;
