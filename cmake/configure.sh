f21800b39464ac6e14c1966d72897e33159c552c
johnnyhuang1007 <johnny@DESKTOP-R2D76QO>
2024-08-07 14:31:31 +0800

    configure-script

---
+++ cmake/configure.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# sudo needed
+if [ "$EUID" -ne 0 ]; then
+  echo "Please use sudo [path]/configure.sh to run the script"
+  exit 1
+fi
+
+OS_TYPE=$(uname -s)
+
+echo "Operating System: $OS_NAME"
+
+if [ "$OS_TYPE" = "Linux" ]; then
+
+  # Linux type
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS_NAME=$ID
+    OS_VERSION=$VERSION_ID
+
+
+    case $OS_NAME in
+      ubuntu|debian)
+        apt install g++
+        apt install make
+        apt install cmake
+        apt install libboost-filesystem-dev
+        apt install libboost-test-dev
+        ;;
+      centos|fedora)
+        echo "安裝必要的軟體包..."
+        #yum update -y
+        #yum install -y curl git vim
+        ;;
+      *)
+        echo "未支持的 Linux 發行版本: $OS_NAME"
+        exit 1
+        ;;
+    esac
+  else
+    echo "/etc/os-release not found."
+    exit 1
+  fi
+
+elif [ "$OS_TYPE" = "Darwin" ]; then
+    if ! command -v brew &> /dev/null; then
+        echo "Homebrew not found. Please make sure you have brew installed"
+        #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    brew install gcc
+    brew install make
+    brew install cmake
+    brew install boost
+else
+  echo "UNSUPPORTED OS: $OS_TYPE"
+  #exit 1
+fi
+
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+cd "$SCRIPT_DIR/.."
+mkdir ./build
+
