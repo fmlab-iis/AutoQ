c2ad5d091ec5ba572e14c34015a19cea04a70f6d
Wei-Lun Tsai <alan23273850@gmail.com>
2025-07-02 16:18:13 +0800

    verify_your_lsta_is_correct

---
+++ cli/verify_your_lsta_is_correct.cc
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <autoq/error.hh>
+#include <autoq/parsing/timbuk_parser.hh>
+#include <autoq/parsing/complex_parser.hh>
+#include <autoq/serialization/timbuk_serializer.hh>
+#include <autoq/aut_description.hh>
+#include <autoq/inclusion.hh>
+#include <autoq/util/util.hh>
+#include <autoq/util/string.hh>
+#include <autoq/complex/ntuple.hh>
+#include <autoq/complex/complex.hh>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <chrono>
+#include <iomanip>
+#include <regex>
+#include <fenv.h>
+#include <csignal>
+#include <fenv_darwin.h>
+#include <CLI11.hpp>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+try {
+    CLI::App app{"AutoQ: An automata-based C++ tool for quantum program verification."};
+    std::string file1, file2;
+    app.add_option("file1", file1, "file1")->required()->type_name("");
+    app.add_option("file2", file2, "file2")->required()->type_name("");
+    CLI11_PARSE(app, argc, argv); // Parse the command-line arguments
+    auto aut1 = ReadAutomaton(file1);
+    auto aut2 = ReadAutomaton(file2);
+    if (aut1 <= aut2 && aut1 >= aut2) {
+        std::cout << "😀" << std::endl;
+    } else {
+        std::cout << "🥹" << std::endl;
+    }
+} catch (AutoQError &e) {
+    std::cout << e.what() << std::endl;
+}
+    return 0;
+}
